0695eaedf94e35cce8001399f7a0c887da6a9476
Azarar321 <azarar321@gmail.com>
2020-10-24 15:16:11 +0530

    branch one done

--- src/test/java/org/prj/TestSample.java
+++ src/test/java/org/prj/TestSample.java
@@ -5,6 +5,9 @@ public class TestSample {
 	public static void main(String[] args) {
 		
 		System.out.println("Default line");
+		
+		System.out.println("branch one update");
+		System.out.println("Branch one");
 	}
 
 }
