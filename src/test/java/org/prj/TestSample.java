cb3ef46d4bc11c488367ba2d69ea6d3c5ebdbf78
Azarar321 <azarar321@gmail.com>
2020-10-24 15:21:14 +0530

    second branch completed

--- src/test/java/org/prj/TestSample.java
+++ src/test/java/org/prj/TestSample.java
@@ -5,6 +5,16 @@ public class TestSample {
 	public static void main(String[] args) {
 		
 		System.out.println("Default line");
+		System.out.println("second branch");
+		System.out.println("brchtwo");
+		System.out.println("brywo");
+		System.out.println("second branch");
+		System.out.println("brchtwo");
+		System.out.println("brywo");
+		System.out.println("second branch");
+		System.out.println("brchtwo");
+		System.out.println("brywo");
+		
 	}
 
 }
